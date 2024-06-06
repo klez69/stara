b627dea361192f99d1185df5c438b3b4fbf5a46e
klez69 <118447126+klez69@users.noreply.github.com>
2024-06-06 09:01:52 +0200

    stara strona

---
+++ gora.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>LPG, autogaz, auto gas - CZAKRAM.PL</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="konwerter" content="Ogonki97" />
+	<meta name="DESCRIPTION" content="Firma Czakram.pl to lider na polskim rynku LPG, autogaz oraz auto gas. SprawdŸ nasze ceny instalacji gazowych. Zapraszamy do zapoznania siê z ofert¹." />
+	<meta name="KEYWORDS" content="autogaz, auto gas, LPG, instalacja gazowa, instalacje gazowe, BRC, auto-gaz, ceny instalacji gazowych"/>
+
+	<link rel="stylesheet" type="text/css" href="style1.css"/>
+	<link rel="SHORTCUT ICON" href="http://www.lpg-brc.pl/brc.ico"/>
+	
+	</head>
+	<body>
---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<? require("gora.php"); ?>
+<? require("menu.php"); ?>
+
+<div id="nav-left"><img src="images/page1_03.png" width="564" height="25" alt="Pasel lewy">
+	
+	
+<center><embed src="images/link2.swf" width='479' height='54' alt='Menu'>
+<img src="images/link11.png" width="479" height="194" alt="Pasel lewy"></embed></center>
+	
+<br><br>
+
+
+<br><br>
+			
+</div>
+						
+<? require("prawy.php"); ?>
+<? require("dol.php"); ?>
