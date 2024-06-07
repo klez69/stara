db40ca073ec1c6767c8d34c905a88cb53e7c8885
klez69 <118447126+klez69@users.noreply.github.com>
2024-06-07 11:36:40 +0200

    poprawki

--- menu.php
+++ menu.php
@@ -1,2 +1,2 @@
 <div id="header">
-<div><embed src="images/logo.swf" width='980' height='343' alt='Menu'></embed></div><br>
+    <div><embed src="images/logo.swf" width='980' height='343' alt='Menu'></embed></div><br>
