21ce0a86d597de42c17a9daf19dc1b678d04dbef
klez69 <118447126+klez69@users.noreply.github.com>
2024-06-10 15:42:05 +0200

    pop

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Language" content="pl" />
+  <meta name="Keywords" content="chip tuning, chiptuning, powerbox, hamownia, tuning, czakram hamownia, engine tuning" />
+  <meta name="Description" content="Tuning elektroniczny samochodów - chiptuning" />
+  <meta name="Robots" content="ALL" />
+  <meta name="Author" content="klez" />
+ <link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" /> 
+ <link rel="stylesheet" href="ona.css" type="text/css" />
+ <link rel="stylesheet" href="css/cennik.css" type="text/css" /> 
+  <script src="images/rollover.js" type="text/javascript"></script>
+  <script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+
+  <title>HAMOWNIA - CZAKRAM</title>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36368255-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script src='js/jquery.min.js'></script>
+<script src='js/jqueryCookieGuard.1.0.min.js'></script>
+<script>
+  $(document).ready(function(){
+    $.cookieguard();
+    $.cookieguard.cookies.add('Facebook', 'datr,lu,c_user,xs', 'Facebook uses cookies to track the sites you visit', true);
+    $.cookieguard.cookies.add('Twitter', 'guest_id,k,pid,twitter_sess', 'Twitter uses cookies to maintain widgets', true);
+    $.cookieguard.cookies.add('ASP.NET Session', 'ASP.NET_SessionId', 'This cookie is used to track important logical information for the smooth operation of the site', true);
+    $.cookieguard.cookies.add('PHP Session', 'PHPSESSID', 'This cookie is used to track important logical information for the smooth operation of the site', true);
+    $.cookieguard.cookies.add('ColdFusion Session', 'CFID', 'This cookie is used to track important logical information for the smooth operation of the site', true);
+    $.cookieguard.cookies.add('Java/JSP Session', 'JSESSIONID', 'This cookie is used to track important logical information for the smooth operation of the site', true);
+    $.cookieguard.cookies.add('Classic ASP Session', 'ASPSESSIONID*', 'This cookie is used to track important logical information for the smooth operation of the site', true);
+    $.cookieguard.cookies.add('Google Analytics', '__utma,__utmb,__utmc,__utmz,__utmv', 'These cookies are used to collect information about how visitors use our site. We use the information to compile reports and to help us improve the site. The cookies collect information in an anonymous form, including the number of visitors to the site, where visitors have come to the site from and the pages they visited.', true);
+    $.cookieguard.cookies.add('Omni­ture SiteCatalyst', 's_vi,s_pers, s_sess', 'These cookies are used to collect information about how visitors use our site. We use the information to compile reports and to help us improve the site. The cookies collect information in an anonymous form, including the number of visitors to the site, where visitors have come to the site from and the pages they visited.', true);
+    $.cookieguard.run();
+  });
+</script>
+</head>
+
+<? require("logo.php"); ?>
+<? require("menu.php"); ?>
+<center>
+<div id="kontener1">
+<table id="Tabela_01" width="921" height="450" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td id="tab1" width="353" height="450">
+			<a href=""><img src="images/p_00.png" onmouseover="src='images/p_00.png'" onmouseout="src='images/p_00.png'" border="0" width="353" height="82" alt="Chipowanie na śniadanie">
+			</a>
+		<img src="images/p_33.png" onmouseover="src='images/p_33.png'" onmouseout="src='images/p_33.png'" border="0" width="353" height="78" >
+				<a href="flota.php"><img src="images/p_04.png" onmouseover="src='images/p_14.png'" onmouseout="src='images/p_04.png'" border="0" width="353" height="77" alt="">
+				</a>
+					<a href="http://vtech.pl/"><img src="images/p_06.png" onmouseover="src='images/p_16.png'" onmouseout="src='images/p_06.png'" border="0" width="353" height="78" alt="V-tech">
+					</a>
+						<a href="http://www.hamownia.czakram.pl/tab_unia.php"><img src="images/p_07.png" onmouseover="src='images/p_17.png'" onmouseout="src='images/p_07.png'" border="0" width="353" height="55" alt="">
+						</a>
+							<a href=""><img src="images/p_00.png" onmouseover="src='images/p_00.png'" onmouseout="src='images/p_00.png'" border="0" width="353" height="70" alt="">
+						</a>
+			
+		</td>
+		<td id="tab2" width="568" height="450">
+			</td>
+	</tr>
+</table>
+	
+
+	
+</div>
+</center>
+<? require("stopka.php"); ?>
